8485579d11eeef2ef95f4381fdc4fb600e0735b6
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-15 19:15:32 +0600

    Displaying Videos from Local Storage

--- app/src/main/java/com/example/video_player_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/video_player_kotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.video_player_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
 import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val videoView : VideoView = findViewById(R.id.videoView1)
+
+        // Video path
+        videoView.setVideoPath("android.resource://" +packageName +"/" +R.raw.video)
+
+        // Media controller
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        mediaController.setMediaPlayer(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.start() // If want to start video when this screen open
 
     }
 }
