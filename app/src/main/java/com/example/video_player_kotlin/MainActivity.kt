63443262ff5afd78c8a732b881d7aa2bae3faeb0
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-15 18:58:05 +0600

    Add videoView

--- app/src/main/java/com/example/video_player_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/video_player_kotlin/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.video_player_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val videoView : VideoView = findViewById(R.id.videoView1)
+
     }
 }
